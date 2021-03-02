c6657b7c9ee1d28e22a700ec8a89f19d10c81d3f
v-bliznyukov <69856961+v-bliznyukov@users.noreply.github.com>
2021-03-03 00:08:30 +0300

    Create task2&explain.sql

---
+++ task2&explain.sql
@@ -0,0 +1,13 @@
+EXPLAIN ANALYZE
+--select the total amount and the store id
+SELECT store_id, SUM(amount) 
+FROM (SELECT *
+	  --table with payment connected to a particular store
+FROM (payment LEFT JOIN staff ON staff.staff_id=payment.staff_id)
+WHERE
+	  --choose payments for the last month
+    date_trunc('month', payment_date) IN (SELECT date_trunc('month', max(payment_date)) FROM payment)) as last_payment
+GROUP BY store_id
+ORDER BY sum DESC;
+
+--HERE THE SORTING IS THE COSTS THE MOST, AGAIN INDEXING CAN HELP (FOR EXAMPLE, SORTIGN WITH B TREE SEARCH IS LOG(2))
